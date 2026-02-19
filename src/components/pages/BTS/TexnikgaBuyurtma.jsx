9896fb6dbd6e9a4b9fb313eb67cd99fa40ea6521
bilolcoder <bilolcoder2009@gmail.com>
2026-02-19 13:29:15 +0500

    joy delete

--- src/components/pages/BTS/TexnikgaBuyurtma.jsx
+++ src/components/pages/BTS/TexnikgaBuyurtma.jsx
@@ -245,8 +245,8 @@ function TexnikgaBuyurtma() {
   };
 
   return (
-    <div className="min-h-screen py-8 sm:px-6 lg:px-8">
-      <div className="max-w-7xl mx-auto">
+    <div className="py-8 sm:px-6 lg:px-8">
+      <div className="">
         <h1 className="text-3xl font-bold text-gray-800 mb-8">Texnikga Buyurtmalar</h1>
 
         {/* Filter */}
